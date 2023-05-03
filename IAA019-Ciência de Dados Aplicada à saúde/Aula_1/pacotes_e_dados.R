2abad512c24a2685bf33ec92863ee17a65865f54
Hobbies-Prof-Bento <clistenes_gb@yahoo.com.br>
2023-05-03 19:14:35 -0300

    feat: add taks

---
+++ IAA019-Ciência de Dados Aplicada à saúde/Aula_1/pacotes_e_dados.R
@@ -0,0 +1,35 @@
+
+#--- R: Linguagem e ambiente de desenvolvimento:
+# https://www.r-project.org/
+  
+#--- RStudio: Software livre de ambiente de desenvolvimento integrado para R:
+# https://posit.co/products/open-source/rstudio/
+
+#--- URL pacotes R:
+# https://bioconductor.org/packages/TreeAndLeaf/
+# https://www.bioconductor.org/packages/RedeR/
+# https://cran.r-project.org/package=RColorBrewer
+# https://cran.r-project.org/package=igraph
+# https://bioconductor.org/packages/SummarizedExperiment/
+# https://bioconductor.org/packages/ComplexHeatmap/
+# https://cran.r-project.org/package=survival
+# https://cran.r-project.org/package=survminer
+# https://cran.r-project.org/package=shiny
+
+# Carregamento dos pacotes em R:
+library(TreeAndLeaf) # Pacote Bioconductor
+library(RedeR) # Pacote Bioconductor
+library(RColorBrewer)  # Pacote CRAN
+library(igraph)  # Pacote CRAN
+library(SummarizedExperiment) # Pacote Bioconductor
+library(ComplexHeatmap) # Pacote Bioconductor
+library(circlize) # Pacote CRAN
+library(RColorBrewer)  # Pacote CRAN
+library(survival)  # Pacote CRAN
+library(survminer)  # Pacote CRAN
+library(shiny)  # Pacote CRAN
+
+#--- Dados
+#https://github.com/csgroen/RTN_example_TCGA_LIHC/
+#https://github.com/csgroen/RTN_example_TCGA_LIHC/raw/master/tcgaLIHCdata_preprocessed.RData
+
